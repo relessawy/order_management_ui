4bc3757ac6146bc92e3096a4727a289e88ea59b1
Krishnan Narayana Swamy <krnaraya@redhat.com>
2021-10-01 12:52:50 +1000

    Krishnan simple combo box

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { KieService } from '../kie.service';
 import { MessageService } from '../message/message.service';
 import { Router } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ComboBoxComponent } from '../combo-box/combo-box.component';
-import { ComboBoxPipe } from '../combo-box/combo-box.pipe';
 
 @Component({
   selector: 'app-start',
@@ -13,23 +9,25 @@ import { ComboBoxPipe } from '../combo-box/combo-box.pipe';
 })
 export class StartComponent implements OnInit {
   filename: string;
-  laptopList: [];
+  selectedLevel;
+  data:Array<Object> = [
+      {id: 0, name: "Macbook Pro"},
+      {id: 1, name: "Surface Pro"},
+      {id: 1, name: "Chromebook"}
+  ];
 
   constructor(private kieService: KieService, private messageService: MessageService, private router: Router) {
-
-    this.laptopList = [
-      { "laptopname": "Macbook Pro", "code": "MP" },
-      { "laptopname": "Surface Pro", "code": "SP" },
-      { "laptopname": "Chromebook", "code": "CB" },
-    ];
-
    }
 
   submit() {
-    this.kieService.startProcess(this.laptopList.laptopname).subscribe(res => {
+    this.kieService.startProcess(this.selectedLevel.name).subscribe(res => {
       this.messageService.success(`Process ${res} started`);
       this.router.navigate(['/home/process']);
     });
+  }
+
+  selected(){
+    alert(this.selectedLevel.name)
   }
 
   ngOnInit() {
