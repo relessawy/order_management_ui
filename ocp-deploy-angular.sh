985c611cc07154f9a651cef064ff43cee90591a1
Krishnan Narayana Swamy <krnaraya@redhat.com>
2021-10-01 12:45:17 +1000

    Krishnan for adding combo box support

--- ocp-deploy-angular.sh
+++ ocp-deploy-angular.sh
@@ -3,6 +3,8 @@
 project_name="rhpam-mod2"
 oc project ${project_name}
 
+​​oc delete all -l app=procurement-process-web
+
 oc new-app --strategy="source" \
 --name="procurement-process-web" \
 --code="https://github.com/relessawy/ocp-pam"
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { KieService } from '../kie.service';
 import { MessageService } from '../message/message.service';
 import { Router } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ComboBoxComponent } from '../combo-box/combo-box.component';
+import { ComboBoxPipe } from '../combo-box/combo-box.pipe';
 
 @Component({
   selector: 'app-start',
@@ -9,11 +13,20 @@ import { Router } from '@angular/router';
 })
 export class StartComponent implements OnInit {
   filename: string;
+  laptopList: [];
 
-  constructor(private kieService: KieService, private messageService: MessageService, private router: Router) { }
+  constructor(private kieService: KieService, private messageService: MessageService, private router: Router) {
+
+    this.laptopList = [
+      { "laptopname": "Macbook Pro", "code": "MP" },
+      { "laptopname": "Surface Pro", "code": "SP" },
+      { "laptopname": "Chromebook", "code": "CB" },
+    ];
+
+   }
 
   submit() {
-    this.kieService.startProcess(this.filename).subscribe(res => {
+    this.kieService.startProcess(this.laptopList.laptopname).subscribe(res => {
       this.messageService.success(`Process ${res} started`);
       this.router.navigate(['/home/process']);
     });
