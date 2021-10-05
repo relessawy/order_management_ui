45316c703f843b5590626b098c4627a6c60fc22a
relessawy <ramy_essawy@yahoo.com>
2021-10-06 08:32:39 +1100

    update tasks format

--- ocp-deploy-angular.sh
+++ ocp-deploy-angular.sh
@@ -6,6 +6,6 @@ oc delete all -l app=procurement-process-web
 
 oc new-app --strategy="source" \
 --name="procurement-process-web" \
---code="https://github.com/relessawy/ocp-pam"
+--code="https://github.com/relessawy/order_management_ui"
 
 oc expose svc/procurement-process-web
