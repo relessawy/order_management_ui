7aee26debd83b0565708f1a53a07b0e6a79376cc
relessawy <ramy_essawy@yahoo.com>
2021-11-08 17:11:38 +1100

    set kie variable

--- ocp-deploy-angular-e.sh
+++ ocp-deploy-angular-e.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-project_name="rhpam-mod1"
+project_name="rhpam-mod2"
 oc project ${project_name}
 oc delete all -l app=order-management-ui
 
 oc new-app --strategy="source" \
 --name="order-management-ui" \
 --code="https://github.com/relessawy/order_management_ui"
--e KIE="http://execserver-rhpam-mod1.apps.cluster-e305.e305.sandbox405.opentlc.com/"
+-e kie=http://kieserver-http-rhpam-mod2.apps.cluster-zp67m.zp67m.sandbox944.opentlc.com/
 
 
 oc expose svc/order-management-ui
